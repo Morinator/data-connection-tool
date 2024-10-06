3b3af1f1f0b8734961e5ae41ace1f07b0e51bb93
moritzgross <moritz.gross@digitalfrontiers.de>
2024-10-06 20:37:36 +0200

    initial commit

---
+++ src/main/kotlin/com/digitalfrontiers/dataconnectiontool/DataConnectionToolApplication.kt
@@ -0,0 +1,11 @@
+package com.digitalfrontiers.dataconnectiontool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataConnectionToolApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataConnectionToolApplication>(*args)
+}
---
+++ src/test/kotlin/com/digitalfrontiers/dataconnectiontool/DataConnectionToolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.digitalfrontiers.dataconnectiontool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataConnectionToolApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
