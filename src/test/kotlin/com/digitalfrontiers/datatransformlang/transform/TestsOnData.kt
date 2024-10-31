a6e73e46c5aea2035362359481e8f2a587d693cd
moritzgross <moritz.gross@digitalfrontiers.de>
2024-10-31 13:23:35 +0100

    create tests

---
+++ src/test/kotlin/com/digitalfrontiers/datatransformlang/transform/TestsOnData.kt
@@ -0,0 +1,149 @@
+package com.digitalfrontiers.datatransformlang.transform
+
+import com.jayway.jsonpath.PathNotFoundException
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class TestsOnData {
+
+    private val sights: Data = listOf(
+        mapOf(
+            "name" to "Eiffel Tower",
+            "height" to 300
+        ),
+        mapOf(
+            "name" to "Statue of Liberty",
+            "height" to 93
+        ),
+        mapOf(
+            "name" to "Big Ben",
+            "height" to 96
+        )
+    )
+
+    private val studentGrades: Data = mapOf(
+        "michael" to 3,
+        "hillary" to 5,
+        "josh" to 1
+    )
+
+    @Test
+    fun `get list of entries by field-name`() {
+
+        // given
+        val spec = ToObject(
+            "heights" to Fetch("$[*].height"),
+        )
+
+        // when
+        val result = applyTransform(sights, spec)
+
+        // then
+        val expected = mapOf(
+            "heights" to listOf(300, 93, 96),
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `get single entry`() {
+
+        // given
+        val spec = Fetch("$[0].name")
+
+        // when
+        val result = applyTransform(sights, spec)
+
+        // then
+        val expected = "Eiffel Tower"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `get constant value along selected values`() {
+
+        // given
+        TODO()
+        val spec = Compose()
+
+        // when
+        val result = applyTransform(sights, spec)
+
+        // then
+        val expected = listOf(
+            mapOf(
+                "name" to "Eiffel Tower",
+                "height" to 300,
+                "unit" to "meters"
+            ),
+            mapOf(
+                "name" to "Statue of Liberty",
+                "height" to 93,
+                "unit" to "meters"
+            ),
+            mapOf(
+                "name" to "Big Ben",
+                "height" to 96,
+                "unit" to "meters"
+            )
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `get first 2 names`() {
+
+        // given
+        val spec = Compose(
+            Fetch("\$[0:2]"),
+            ForEach(
+                ToObject(
+                    "name" to Fetch("name")
+                )
+            )
+        )
+
+        // when
+        val result = applyTransform(sights, spec)
+
+        // then
+        val expected = listOf(
+            mapOf("name" to "Eiffel Tower"),
+            mapOf("name" to "Statue of Liberty"),
+        )
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `error on invalid path`() {
+
+        // given
+        val spec = Fetch("$[0].qewqrwettcnbvcn")
+
+        // when & then
+        assertThrows<PathNotFoundException> {
+            applyTransform(sights, spec)
+        }
+    }
+
+    /**
+     * This test checks that "ForEach" only works on list, and returns an empty list per default on all other types.
+     */
+    @Test
+    fun `foreach on non-list returns empty list`() {
+
+        // given
+        val spec = ForEach(
+            Fetch("josh"), // no effect
+        )
+
+        // when
+        val result = applyTransform(studentGrades, spec)
+
+        // then
+        val expected = emptyList<Data>()
+        assertEquals(expected, result)
+    }
+
+}
