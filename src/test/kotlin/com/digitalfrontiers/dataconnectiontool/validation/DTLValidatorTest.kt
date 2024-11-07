48c2dbaf24c6e477448bb137971fd18712daad88
moritzgross <moritz.gross@digitalfrontiers.de>
2024-11-07 15:21:24 +0100

    fix method name add a test for assertFalse

--- src/test/kotlin/com/digitalfrontiers/dataconnectiontool/validation/DTLValidatorTest.kt
+++ src/test/kotlin/com/digitalfrontiers/dataconnectiontool/validation/DTLValidatorTest.kt
@@ -2,14 +2,13 @@ package com.digitalfrontiers.dataconnectiontool.validation
 
 import com.digitalfrontiers.datatransformlang.transform.Specification
 import com.digitalfrontiers.datatransformlang.transform.Specification.Fetch
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class DTLValidatorTest {
 
-    @Test
-    fun testConstTransform() {
-        val schema: String = """
+    private val schema: String = """
         {
           "type": "object",
           "properties": {
@@ -25,12 +24,26 @@ class DTLValidatorTest {
         }
     """.trimIndent()
 
+    @Test
+    fun `test valid transformation`() {
+
         val transform = Specification.ToObject(
             "name" to Fetch("bli"),
             "age" to Fetch("bla"),
         )
 
         assertTrue(DTLValidator.compareDTLWithJSONSchema(schema, transform))
+    }
+
+    @Test
+    fun `test invalid transformation`() {
+
+        val transform = Specification.ToObject(
+            "name" to Fetch("bli"),
+        )
+
+        // a field is missing in the transform spec
+        assertFalse(DTLValidator.compareDTLWithJSONSchema(schema, transform))
     }
 }
 
