1146ce96116a2d95ecd6c08368a4f9e7eca81cbb
Erich Wittenbeck <erich.wittenbeck@digitalfrontiers.de>
2024-11-13 19:35:39 +0100

    feat: add unit test for one of the controllers

--- src/main/kotlin/com/digitalfrontiers/dataconnectiontool/controller/JSONTransformController.kt
+++ src/main/kotlin/com/digitalfrontiers/dataconnectiontool/controller/JSONTransformController.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/transforms", consumes = [MediaType.APPLICATION_JSON_VALUE])
 class JSONTransformController(
     @Autowired private val storage: IStorageService<String>,
-    @Autowired private val transformer: ITransformationService<String, String>,
+    @Autowired private val transformer: ITransformationService<String, String>
 ) {
     private val keyPrefix: String = "specs/"
 
