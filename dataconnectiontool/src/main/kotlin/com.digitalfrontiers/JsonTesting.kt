401f6f0bb5da5f2e2ade66ddb014f2dc94322943
moritzgross <moritz.gross@digitalfrontiers.de>
2024-11-28 14:59:30 +0100

    add rudimentary JSONSink and use it in main function

--- dataconnectiontool/src/main/kotlin/com.digitalfrontiers/JsonTesting.kt
+++ dataconnectiontool/src/main/kotlin/com.digitalfrontiers/JsonTesting.kt
@@ -1,20 +1,26 @@
 package com.digitalfrontiers
 
+import com.digitalfrontiers.components.JSONSink
 import com.digitalfrontiers.components.JSONSource
 import com.digitalfrontiers.transform.Input
 import com.digitalfrontiers.transform.Specification
 
 fun main() {
     val jsonSource = JSONSource()
+    val jsonSink = JSONSink()
 
     val flattenedMap = jsonSource.fetch()
-    println(flattenedMap)
+    println("flattenedMap:      $flattenedMap")
 
     val spec = Specification.Record {
         "john_name" to Input("name")
         "father_name" to Input("['father.name']")
     }
 
-    val result = (Transform to { spec }).apply(flattenedMap)
-    println(result)
+    val transformed: Map<String, String> = (Transform to { spec }).apply(flattenedMap) as Map<String, String>
+
+    println("transformed data : $transformed")
+
+    jsonSink.put(transformed)
+
 }
--- dataconnectiontool/src/main/kotlin/com.digitalfrontiers/components/Sinks.kt
+++ dataconnectiontool/src/main/kotlin/com.digitalfrontiers/components/Sinks.kt
@@ -1,7 +1,9 @@
 package com.digitalfrontiers.components
 
 import com.digitalfrontiers.Format
+import com.digitalfrontiers.transform.convert.defaults.JSONSerializer
 import org.springframework.stereotype.Component
+import java.io.File
 
 // ============================
 
@@ -17,5 +19,29 @@ class DummySink: ISink {
 
     override fun put(data: Map<String, String>) {
         return
+    }
+}
+
+@Component
+class JSONSink: ISink {
+
+    private val js = JSONSerializer()
+    private val filePath = "dummy_data/json/john_doe_transformed.json"
+
+
+    override val id = "JSONSink"
+
+    override val format =
+        Format(
+            listOf(),
+            listOf()
+        )
+
+    override fun put(data: Map<String, String>) {
+        val str = js.serialize(data)
+
+        val file = File(filePath)
+        file.parentFile.mkdirs()
+        file.writeText(str)
     }
 }
