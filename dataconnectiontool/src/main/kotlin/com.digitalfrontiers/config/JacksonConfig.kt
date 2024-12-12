8b66a56ad1193d29c22157859a50513b923a898d
moritzgross <moritz.gross@digitalfrontiers.de>
2024-12-12 11:35:18 +0100

    refactoring

--- dataconnectiontool/src/main/kotlin/com.digitalfrontiers/config/JacksonConfig.kt
+++ dataconnectiontool/src/main/kotlin/com.digitalfrontiers/config/JacksonConfig.kt
@@ -2,7 +2,8 @@ package com.digitalfrontiers.config
 
 
 import com.digitalfrontiers.transform.Specification
-import com.digitalfrontiers.persistence.SpecificationMixin
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Configuration
 
@@ -13,3 +14,21 @@ class JacksonConfig : Jackson2ObjectMapperBuilderCustomizer {
     }
 }
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = Specification.Self::class, name = "Self"),
+    JsonSubTypes.Type(value = Specification.Const::class, name = "Const"),
+    JsonSubTypes.Type(value = Specification.Input::class, name = "Input"),
+    JsonSubTypes.Type(value = Specification.Tuple::class, name = "Tuple"),
+    JsonSubTypes.Type(value = Specification.Record::class, name = "Record"),
+    JsonSubTypes.Type(value = Specification.ListOf::class, name = "ListOf"),
+    JsonSubTypes.Type(value = Specification.Extension::class, name = "Extension"),
+    JsonSubTypes.Type(value = Specification.Rename::class, name = "Rename"),
+    JsonSubTypes.Type(value = Specification.ResultOf::class, name = "ResultOf"),
+    JsonSubTypes.Type(value = Specification.Compose::class, name = "Compose")
+)
+abstract class SpecificationMixin
